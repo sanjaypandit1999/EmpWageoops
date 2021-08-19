50e96f710428280a4aa7132380dd898dd0214f0e
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-08-19 11:58:35 +0530

    REFACTOR:Add_PartTime_Employee

--- src/com/bridgelabz/EmpWageBuild.java
+++ src/com/bridgelabz/EmpWageBuild.java
@@ -3,11 +3,8 @@ package com.bridgelabz;
 public class EmpWageBuild {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-
-        CalEmpWage attendanceCheck = new CalEmpWage();
+        //creat object for call the methods
         CalEmpWage empWageCalculator = new CalEmpWage();
-
-        attendanceCheck.attendanceCheck();
         int workingHrPerDay = empWageCalculator.attendanceCheck();
         empWageCalculator.dailyWage(workingHrPerDay);
     }
