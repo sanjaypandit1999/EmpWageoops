b503b0e6495f334fb6be0aa7248d2c1673846050
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-08-19 17:02:12 +0530

    REFACTOR:Use_Class_Method_compute_empWage

--- src/com/bridgelabz/CalEmpWage.java
+++ src/com/bridgelabz/CalEmpWage.java
@@ -8,23 +8,25 @@ public class CalEmpWage {
      * @version 2.3
      * @since 18-08-2021
      */
-    public static final int IS_PRESENT = 1, IS_PART_TIME_PRESENT = 2, WORKING_DAY_FOR_MONTH = 20;;
-
+    //declare instance variable
+    public static final int IS_PRESENT = 1, IS_PART_TIME_PRESENT = 2, WORKING_DAY_FOR_MONTH = 20;
+    //Creat Method For UC1
     public static int attendanceCheck() {
-        int workingHrPerDay = 0 ;
-        double empRandomCheck = Math.floor(Math.random()*10)%3;
+        int workingHrPerDay = 0;
+        double empRandomCheck = Math.floor(Math.random() * 10) % 3;
         if (empRandomCheck == IS_PRESENT) {
             workingHrPerDay = 8;
             System.out.println("Employee is Present");
-        }else if (empRandomCheck == IS_PART_TIME_PRESENT) {
+        } else if (empRandomCheck == IS_PART_TIME_PRESENT) {
             System.out.println("Employee is part time ");
             workingHrPerDay = 4;
-        }else{
-            workingHrPerDay= 0;
+        } else {
+            workingHrPerDay = 0;
             System.out.println("Employee is Absent");
         }
         return workingHrPerDay;
     }
+    //For Uc3
     public int dailyWage(int workingHrPerDay) {
 
         int WAGE_PER_HRS = 20;
@@ -34,11 +36,13 @@ public class CalEmpWage {
 
         return dailyWage;
     }
+
     /**
      * Purpose - checks attendance and calculate working per hr using case switch method
      *
      * @return workingHrPerDay (8hr, 4hr or 0hr)
      */
+    //For uc5 problem solvewith using switch case
     public int switchCase() {
 
         int workingHrPerDay;
@@ -62,11 +66,13 @@ public class CalEmpWage {
         }
         return workingHrPerDay;
     }
+
     /**
      * Purpose - Calculate wage for a month
      *
      * @return monthly wage
      */
+    //UC6 calculate monthly wages
     public int monthlyWage() {
         CalEmpWage monthlySalary = new CalEmpWage();
 
@@ -81,11 +87,10 @@ public class CalEmpWage {
             monthlyWage += dailyWage;
 
         }
-        if (workHrPerMonth<=100){
-            System.out.println("Emlpoyee working hours is " +workHrPerMonth);
-        }
-        else
-            System.out.println("yor are work maximum hours for the month your working hours is " +workHrPerMonth);
+        if (workHrPerMonth <= 100) {
+            System.out.println("Emlpoyee working hours is " + workHrPerMonth);
+        } else
+            System.out.println("yor are work maximum hours for the month your working hours is " + workHrPerMonth);
         return monthlyWage;
     }
 }
